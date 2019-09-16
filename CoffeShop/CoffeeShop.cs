dbb7ba08a9311f1b29ac9c98368219c694b8d780
Afsarud <engafsar5577@gmail.com>
2019-09-16 19:10:49 +0600

    Array

--- CoffeShop/CoffeeShop.cs
+++ CoffeShop/CoffeeShop.cs
@@ -22,7 +22,6 @@ namespace CoffeShop
 
         private void button1_Click(object sender, EventArgs e)
         {
-            
             string order = orderComboBox.Text;
             int productQuantity = Convert.ToInt32(quantityTextBox.Text);
             int ProAmount = 0;
@@ -43,11 +42,13 @@ namespace CoffeShop
             {
                 ProAmount = 80;
             }
+
             int totalPrice = ProAmount * productQuantity;
-            //string message = "";
-            const int num = 10;
+
+            const int num = 1;
             string[,] name = new string[10,6];
             for(int i = 0; i < num; i++)
+
             {
                 name[i,0] = Convert.ToString(CusNameTextBox.Text);
                 name[i,1] = Convert.ToString(contactTextBox.Text);
@@ -55,27 +56,14 @@ namespace CoffeShop
                 name[i,3] = Convert.ToString(orderComboBox.SelectedItem);
                 name[i,4] = Convert.ToString(quantityTextBox.Text);
                 name[i,5] = Convert.ToString(totalPrice);
-            
-
-            
-                richTextBox1.Text = "Customer Name: " + name[i,0] + "\n" +
+             
+                richTextBox1.Text += "\n Customer Name: " + name[i,0] + "\n" +
                "Contact Number: " + name[i,1] + "\n" +
                "Address: " + name[i,2] + "\n " +
                "Order Amount:" + name[i,3] + "\n" +
                "Quantity Value: " + name[i,4] + "\n" +
-               "Total Price: " + name[i,5] + "\n";
+               "Total Price: " + name[i,5] + "\n \n";
             }
-
-           
-
-
-            //richTextBox1.Text = "Customer Name: " + CusNameTextBox.Text + "\n" +
-            //   "Contact Number: " + contactTextBox.Text + "\n" +
-            //   "Address: " + addressTextBox.Text + "\n " +
-            //   "Order Amount:" + orderComboBox.SelectedItem + "\n" +
-            //   "Quantity Value: " + quantityTextBox.Text + "\n" +
-            //   "Total Price: " + totalPrice;
-
         }
         private void richTextBox1_TextChanged(object sender, EventArgs e)
         {
